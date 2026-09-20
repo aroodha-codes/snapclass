3d3bea1fc865c911122ec33d9959f74896c69baf
aroodha-codes <aroodha27@gmail.com>
2026-09-20 23:16:43 +0530

    update header and footer

--- src/components/footer.py
+++ src/components/footer.py
@@ -1,25 +1,13 @@
 import streamlit as st
 
-
 def footer_home():
-    logo_url = "https://i.ibb.co/4r5X1FY/apnacollege.png"
-    
-    st.markdown(f"""
-        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
-        <p style="font-weight:bold; color:white;"> Created with ❤️ by </p>  
-        <img src='{logo_url}' style='max-height:25px' />
-        </div>
-                
-                """, unsafe_allow_html=True)
-
+    st.markdown(
+        "<div style='text-align:center; margin-top:2rem; color:#E0E3FF;'>SnapClass • AI-Powered Smart Attendance</div>",
+        unsafe_allow_html=True
+    )
 
 def footer_dashboard():
-    logo_url = "https://i.ibb.co/4r5X1FY/apnacollege.png"
-    
-    st.markdown(f"""
-        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
-        <p style="font-weight:bold; color:black;"> Created with ❤️ by </p>  
-        <img src='{logo_url}' style='max-height:25px' />
-        </div>
-                
-                """, unsafe_allow_html=True)
+    st.markdown(
+        "<div style='text-align:center; margin-top:2rem; color:#666;'>SnapClass • AI-Powered Smart Attendance</div>",
+        unsafe_allow_html=True
+    )
