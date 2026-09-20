f50068d938612ce18630d1686c18d528f197be5a
aroodha-codes <aroodha27@gmail.com>
2026-09-21 00:23:08 +0530

    fix: guard attendance inputs and isolate subject actions

--- src/screens/student_screen.py
+++ src/screens/student_screen.py
@@ -67,10 +67,10 @@ def student_dashboard():
 
         stats = stats_map.get(sid,{"total":0, "attended": 0} )
         def unenroll_button():
-                if st.button("Unenroll from tihs course", type='tertiary', width='stretch', icon=':material/delete_forever:'):
-                    unenroll_student_to_subject(student_id, sid)
-                    st.toast(f'Unenrolled from {sub['name']} successfully!')
-                    st.rerun()
+            if st.button("Unenroll from tihs course",key=f"unenroll_{student_id}_{sid}",type="tertiary",width="stretch",icon=":material/delete_forever:",):
+                unenroll_student_to_subject(student_id, sid)
+                st.toast(f'Unenrolled from {sub['name']} successfully!')
+                st.rerun()
 
         with cols[i % 2]:
 
